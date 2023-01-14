42511a4b272e362ea8b9e2a221bc2daff2fb62de
Filimon4 <f.rykov@bk.ru>
2023-01-14 14:03:37 +0300

    add handlers

---
+++ data/handlers/main.py
@@ -0,0 +1,11 @@
+from aiogram import Dispatcher
+
+#import other
+
+def register_modules(dp: Dispatcher):
+    print('hi')
+    handlers = (
+
+    )
+    for handler in handlers:
+        handler(dp)
--- data/main.py
+++ data/main.py
@@ -1,5 +1,12 @@
 from aiogram import Bot, Dispatcher, executor, types
 import os
 
+from .handlers import register_modules
+
+async def create_logic(dp: Dispatcher):
+    register_modules(dp)
+
 def start_bot():
-    print(os.getenv('TOKEN'))
+    bot = Bot(token=os.getenv("TOKEN"))
+    dp = Dispatcher(bot)
+    executor.start_polling(dp, skip_updates=True, on_startup=create_logic)
