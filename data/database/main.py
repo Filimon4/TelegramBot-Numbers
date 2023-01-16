e62bd1ac62d02b91707a65e8216080c3b217c7dd
Filimon4 <f.rykov@bk.ru>
2023-01-16 13:43:00 +0300

    peewee test

--- data/database/main.py
+++ data/database/main.py
@@ -1,13 +1,7 @@
 from peewee import *
-from .models.user import *
+from .module.user import *
 
 def register_database():
-    db.connect()
-    with db:
-        db.create_tables([User, Pets])
-        user = User.get(User.id == 2)
-        print(user)
-    print("Done")
-    db.close()
+    pass
 
 
