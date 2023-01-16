62e2ee1f85b1e7eb67adc8aa822228a77c31a51d
Filimon4 <f.rykov@bk.ru>
2023-01-16 13:55:09 +0300

    del a class

--- data/database/models/user.py
+++ data/database/models/user.py
@@ -13,11 +13,3 @@ class User(BaseModel):
 
     class Meta:
         db_table = "User"
-
-class Pets(BaseModel):
-    owner = ForeignKeyField(User, backref='pets')
-    name = CharField()
-    animal_type = CharField()
-
-    class Meta:
-        db_table = "Pets"
