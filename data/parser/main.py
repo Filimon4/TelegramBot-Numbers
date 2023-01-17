a3aa00a72b63fb243f2744fecc39dbe8ddf7c63d
Filimon4 <f.rykov@bk.ru>
2023-01-17 10:33:06 +0300

    chnages

--- data/parser/main.py
+++ data/parser/main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 def get_data(url):
     headers = {
-        "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+        "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0"
     }
 
     req = requests.get(url, headers)
