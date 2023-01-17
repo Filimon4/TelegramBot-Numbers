a33d1ea3562db3afb8b063e5f4d3f8f210311b9e
Deumir404 <deumir@yandex.ru>
2023-01-17 13:46:21 +0300

    buttons_voicek

--- data/handlers/utils.py
+++ data/handlers/utils.py
@@ -1,12 +1,28 @@
 from aiogram import Dispatcher, types
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, Voice
 from .Inline_kb_menu import ikb_menu
 from .bkb_menu import kb_menu, kb_menu2
+import SpeechRecognition as sr
+r = sr.Reconginzer()
 
 
 
 async def errors(mes: Message):
     await mes.answer(f'Command {mes.text} not found!')
+
+async def Listened():
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listened(source)
+        try :
+            text = r.recongnize_google(audio, language= 'ru-Ru')
+        except sr.UnknownValueError:
+            pass
+        print(text)
+        return text
+
+async def Voice_answer(message: types.message):
+    await  message.answer ("Listen",  ) 
 
 async def show_inline_menu(message: types.Message):
     await  message.answer ("Инлайн кнопки ниже", reply_markup=ikb_menu) 
@@ -40,7 +56,8 @@ async def menu(message: types.Message):
 
 def regirst_events(dp: Dispatcher):
     # register events
-    
+
+    dp.register_message_handler(Voice_answer, content_types =  Voice )
     dp.register_message_handler(show_inline_menu, commands = 'menu')
     dp.register_message_handler(menu, commands = 'kmenu')
     dp.register_callback_query_handler(send_message_ikb, text = 'Your text')
@@ -54,4 +71,5 @@ def regirst_events(dp: Dispatcher):
     dp.register_message_handler(back_message, text= 'go'   )
     dp.register_message_handler(back_message, text= 'back'   )
     dp.register_message_handler(errors, content_types=['text'])
+
     
