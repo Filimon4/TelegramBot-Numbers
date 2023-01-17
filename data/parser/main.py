81e214efdbe8cdf254066c4d89d8fbbb52817c78
CodeWAds <98021790+CodeWAds@users.noreply.github.com>
2023-01-17 22:37:37 +0300

    pars img

--- data/parser/main.py
+++ data/parser/main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-
 def get_data(url):
     headers = {
         "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
@@ -9,30 +8,32 @@ def get_data(url):
 
     # with open('index.html','r', encoding='utf-8') as file:
     #     src = file.read()
-    src = requests.get(url, headers)
+    src = requests.get(url).content
+
+    
     
 
-    soup = BeautifulSoup(src.text, 'lxml')
+    soup = BeautifulSoup(src, 'lxml')
     movies = soup.find_all(class_="EventList__Event-sc-14wck6-3 dKUEol event rental large")
     dictionary = {}
     for movie in movies:
         print('--------------------------------')
-        soupMovie = BeautifulSoup(src, 'lxml')
+        movie = BeautifulSoup(src, 'lxml')
         name = movie.find_all('a', class_='event-name')
         for name_ in name:
             text_name = name_.text
             text_name = text_name.replace("  ", "")
-            print(text_name)
             
 
-    
+        text_time_session  = []
         time_session = movie.find_all(class_='Show-sc-rhge3d-2 gDYwMv show')
-        text_time_session = []
+        
         for time_session_ in time_session:
             text_time_session.append(time_session_.text)
-            print(text_time_session)
+            
 
         dictionary[text_name] = text_time_session
+        print(text_time_session)
 
     soup = BeautifulSoup(src, 'lxml')
     movies_pk = soup.find_all(class_="EventList__Event-sc-14wck6-3 dKUEol event rental pushkin-card large")
@@ -51,14 +52,18 @@ def get_data(url):
 
             print(time_session_.text)
         
+        image_string = ''
         image = movie_pk.find('div',class_='Poster-sc-mn9zap-1 kAKPQC event-poster').find('img').get('src')
         for image_text in image:
-            image_text = image.text
-            print(image_text)
+            image_string += image_text
+            image_string = image_string.replace('22x32/filters:quality(80):blur(2)/','')
+        print(image_string)
+            
+            
             
 
 
-        dictionary[text_name] = text_time_session
+        dictionary[text_name] = text_time_session, 
 
         
         
@@ -68,4 +73,4 @@ def get_data(url):
 
 
 
-get_data(f'https://kinosmena.ru/?date=2023/01/{17}')
+get_data(f'https://kinosmena.ru/')
