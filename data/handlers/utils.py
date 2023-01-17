fa3ff17612a9cb8bfd6ab6c21ca1b010f0be9485
Deumir404 <deumir@yandex.ru>
2023-01-17 22:16:28 +0300

    buttons_voicev3

--- data/handlers/utils.py
+++ data/handlers/utils.py
@@ -1,9 +1,11 @@
 from aiogram import Dispatcher, types, Bot
-from aiogram.types import Message, CallbackQuery, Voice
+from aiogram.types import ContentType, Message, CallbackQuery, Voice, File
 from .Inline_kb_menu import ikb_menu
 from .bkb_menu import kb_menu, kb_menu2
 import speech_recognition as sr
 from pathlib import Path
+import os
+
 
 
 
@@ -11,7 +13,7 @@ from pathlib import Path
 async def errors(mes: Message):
     await mes.answer(f'Command {mes.text} not found!')
 
-async def Listened(file_name):
+def Listened(file_name):
     with sr.AudioFile(file_name) as source:
         audio = r.record(source)
         sr.Recognizer.adjust_for_ambient_noise(source)
@@ -19,20 +21,23 @@ async def Listened(file_name):
         try :
             text = sr.Recognizer.recongnize_google(audio, language= 'ru-Ru')
         except sr.UnknownValueError:
-            pass
-        print(text)
-        return text
+            text = ('i dont know')
+        return (text)
 
 async def handle_file(file: File, file_name: str, path: str):
     Path(f"{path}").mkdir(parents=True, exist_ok=True)
 
-    await Bot.download_file(file_path=file.file_path, destination=f"{path}/{file_name}")
+    await Bot(token=os.getenv("TOKEN")).download_file(file_path=file.file_path, destination=f"{path}/{file_name}")
 
 async def Voice_answer(msg: types.Message):
     voice = await msg.voice.get_file()
-    path = "./voices_answer"
-    await handle_file( file_name=f"{voice.file_id}.ogg", path=path)
-    await msg.reply(Listened(msg.audio))
+    file_name = f"{voice.file_id}.ogg"
+    path = './handler/voice_answer'
+    await handle_file(file = voice, file_name=f"{voice.file_id}.ogg", path=path)
+    File_voice = str(Path(f'./handler/voice_answer/{file_name}')) 
+    await msg.reply(f'Секунду {File_voice} ')
+    await msg.reply(Listened(File_voice))
+    
 
 async def show_inline_menu(message: types.Message):
     await  message.answer ("Инлайн кнопки ниже", reply_markup=ikb_menu) 
@@ -67,7 +72,7 @@ async def menu(message: types.Message):
 def regirst_events(dp: Dispatcher):
     # register events
 
-    dp.register_message_handler(Voice_answer, content_types=types.ContentTypes.VOICE)
+    dp.register_message_handler(Voice_answer, content_types=[ContentType.VOICE])
     dp.register_message_handler(show_inline_menu, commands = 'menu')
     dp.register_message_handler(menu, commands = 'kmenu')
     dp.register_callback_query_handler(send_message_ikb, text = 'Your text')
