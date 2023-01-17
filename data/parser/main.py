a6bd6c63483784d910c9f8444702afb1fb57004c
CodeWAds <98021790+CodeWAds@users.noreply.github.com>
2023-01-17 10:32:04 +0300

    parser

--- data/parser/main.py
+++ data/parser/main.py
@@ -1,20 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 def get_data(url):
     headers = {
         "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
     }
 
-    req = requests.get(url, headers)
-    print(req.text)
-    # with open('index.html', 'w') as file:
-    #     file.write(req.text)
+    with open('index.html','r', encoding='utf-8') as file:
+        src = file.read()
+    # src = requests.get(url, headers)
+    # soup = BeautifulSoup(src.text, 'lxml')
 
 
-    # with open('index.html') as file:
-    #     src = file.read()
+    soup = BeautifulSoup(src, 'lxml')
+    name = soup.find_all('a', class_='event-name')
+    for name_ in name:
+        text_name = name_.text
+        text_name = text_name.replace("  ", "")
+        
 
-    # soup = BeautifulSoup(src, 'lxml')
-    # articles = soup.find_all()
-get_data('https://kolizeum.ru/')
+    
+    date_tab = soup.find_all(class_="day-tab")
+    for date_tab_ in date_tab:
+        text_date_tab = date_tab_.text
+        match = re.match(r"([a-z]+)([0-9]+)", text_date_tab, re.I)
+        if match:
+            text_date_tab = match.groups()
+        
+            print(text_date_tab)
+
+
+get_data('https://kinosmena.ru/')
