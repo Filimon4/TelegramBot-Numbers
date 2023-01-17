40ad0fb188e305141ed2671fd40c1f3dcbf77cfc
Filimon4 <f.rykov@bk.ru>
2023-01-17 11:50:45 +0300

    some thanges

--- data/parser/main.py
+++ data/parser/main.py
@@ -14,30 +14,40 @@ def get_data(url):
     # soup = BeautifulSoup(s    rc.text, 'lxml')
 
     soup = BeautifulSoup(src, 'lxml')
-    movies = soup.find_all(class_="EventList__Event-sc-14wck6-3 dKUEol event rental large")
-    for movie in movies:
-        with open("movie.html", 'r', encoding='utf-8') as f:
-             moiveHtml = f.read()
-        print('--------------------------------')
-        soupMovie = BeautifulSoup(moiveHtml, 'lxml')
-        name = soupMovie.find_all('a', class_='event-name')
-        for name_ in name:
-            text_name = name_.text
-            text_name = text_name.replace("  ", "")
-            print(text_name)   
+    # movies = soup.find("div", class_="EventList__EventListWrap-sc-14wck6-2 kxVNGK")
+    # with open("movies.html", 'w', encoding='utf-8') as f:
+    #     f.write(str(movies))
+
+    movies = soup.find_all('div', class_="sc-bczRLJ sc-gsnTZi bOvxGB jnFvAE event-info")
+    print(movies)
+    with open("movieEvery.html", 'w', encoding='utf-8') as f:
+        f.write(str(movies))
+
+    # for movie in movies:
+
+
+    #     with open("movie.html", 'r', encoding='utf-8') as f:
+    #          moiveHtml = f.read()
+    #     print('--------------------------------')
+    #     soupMovie = BeautifulSoup(moiveHtml, 'lxml')
+    #     name = soupMovie.find_all('a', class_='event-name')
+    #     for name_ in name:
+    #         text_name = name_.text
+    #         text_name = text_name.replace("  ", "")
+    #         print(text_name)   
 
     
-        date_tab = soupMovie.find_all(class_="day-tab")
-        for date_tab_ in date_tab:
-            text_date_tab = date_tab_.text
+    #     date_tab = soupMovie.find_all(class_="day-tab")
+    #     for date_tab_ in date_tab:
+    #         text_date_tab = date_tab_.text
     
-            print(text_date_tab)
+    #         print(text_date_tab)
     
-        time_session = soupMovie.find_all(class_='show-time')
-        for time_session_ in time_session:
-            text_time_session = time_session_.text
+    #     time_session = soupMovie.find_all(class_='show-time')
+    #     for time_session_ in time_session:
+    #         text_time_session = time_session_.text
 
-            print(text_time_session)
+    #         print(text_time_session)
 
 
 
