de6f469e56f967dac37f250d4a9e8e520f50863c
Filimon4 <f.rykov@bk.ru>
2023-01-17 22:44:22 +0300

    parser remeake

--- data/parser/main.py
+++ data/parser/main.py
@@ -7,65 +7,49 @@ def get_data(url):
         "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0"
     }
 
-    # with open('index.html','r', encoding='utf-8') as file:
-    #     src = file.read()
-    src = requests.get(url, headers)
-    
+    with open('index.html','r', encoding='utf-8') as file:
+        src = file.read()
+    # src = requests.get(url, headers)
 
-    soup = BeautifulSoup(src.text, 'lxml')
+    soup = BeautifulSoup(src, 'lxml')
+
     movies = soup.find_all(class_="EventList__Event-sc-14wck6-3 dKUEol event rental large")
+    movies_pk = soup.find_all(class_="EventList__Event-sc-14wck6-3 dKUEol event rental pushkin-card large")
+
     dictionary = {}
-    for movie in movies:
-        print('--------------------------------')
+    for movie in (movies+movies_pk):
         soupMovie = BeautifulSoup(src, 'lxml')
+
+        text_time_session = {}
+
         name = movie.find_all('a', class_='event-name')
         for name_ in name:
             text_name = name_.text
             text_name = text_name.replace("  ", "")
-            print(text_name)
-            
-
-    
-        time_session = movie.find_all(class_='Show-sc-rhge3d-2 gDYwMv show')
-        text_time_session = []
+            text_name = text_name.replace('\n', " ")
+        
+        time_session = movie.find_all("div", class_='EventSchedule__Schedule-sc-1crhtam-0 fgDpKR schedule')
         for time_session_ in time_session:
-            text_time_session.append(time_session_.text)
-            print(text_time_session)
+            container = time_session_.find_all('div', class_='facility')
+            for obj in container:
+                sinema_name = obj.find("span", class_="facility-address")
+                branch_data = obj.find_all('div', class_='Show-sc-rhge3d-2 gDYwMv show')
 
-        dictionary[text_name] = text_time_session
+                text_time_session.update({sinema_name.text:{}})
 
-    soup = BeautifulSoup(src, 'lxml')
-    movies_pk = soup.find_all(class_="EventList__Event-sc-14wck6-3 dKUEol event rental pushkin-card large")
-    for movie_pk in movies_pk:
-        print('--------------------------------')
-        name = movie_pk.find_all('a', class_='event-name')
-        for name_ in name:
-            text_name = name_.text
-            text_name = text_name.replace("  ","")
-            print(text_name)   
-    
-        time_session = movie_pk.find_all(class_='Show-sc-rhge3d-2 gDYwMv show')
-        text_time_session = []
-        for time_session_ in time_session:
-            text_time_session.append(time_session_.text)
+            for data_session in branch_data:
+                time = data_session.find('div', class_ = "show-time")
+                cost = data_session.find('div', class_ = "Show__Price-sc-rhge3d-0 kmWvJz price")
+                
+                time_text = time.text.strip()
+                cost_text = cost.text.strip()
 
-            print(time_session_.text)
-        
-        image = movie_pk.find('div',class_='Poster-sc-mn9zap-1 kAKPQC event-poster').find('img').get('src')
-        for image_text in image:
-            image_text = image.text
-            print(image_text)
-            
+                text_time_session[sinema_name.text].update({time_text: cost_text})
 
-
-        dictionary[text_name] = text_time_session
-
-        
-        
+        dictionary.update({text_name : text_time_session})
+        print(dictionary)
     json_object = json.dumps(dictionary, indent=4, ensure_ascii=False)
-    with open("sample.json", "w", encoding='utf-8') as outfile:
+    with open("sample2.json", "w", encoding='utf-8') as outfile:
         outfile.write(json_object)
 
-
-
-get_data(f'https://kinosmena.ru/?date=2023/01/{17}')
+get_data(f'https://kinosmena.ru/?date=2023/01/{18}&facility=smena')
