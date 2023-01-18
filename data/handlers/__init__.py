6676d9c254c772e8fa09c8a9cb9d13a350821329
CodeWAds <98021790+CodeWAds@users.noreply.github.com>
2023-01-18 14:23:39 +0300

    req

--- data/handlers/__init__.py
+++ data/handlers/__init__.py
@@ -1,4 +1,4 @@
-from .main import register_modules
+from .main import registr_modules
 # from .utils import show_inline_menu
 # from .Inline_kb_menu import ikb_menu
 # from .bkb_menu import kb_menu, kb_menu2
