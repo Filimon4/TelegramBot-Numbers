c351cc95683936e0724c6fd6beb5e5d6e1245778
Filimon4 <f.rykov@bk.ru>
2023-01-18 08:41:43 +0300

    add smt

--- data/main.py
+++ data/main.py
@@ -1,10 +1,12 @@
 from aiogram import Bot, Dispatcher, executor, types
 
 from .handlers import registr_modules
-from .bot import bot,dp
+# from .bot import bot,dp
 
 async def create_logic(dp: Dispatcher):
     registr_modules(dp)
 
 def start_bot():
+    bot = Bot(token=os.getenv("TOKEN"))
+    bp = Dispatcher(bot)
     executor.start_polling(dp, skip_updates=True, on_startup=create_logic)
